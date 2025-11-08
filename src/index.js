538c740e4c6c46e9e406f6ba2210c02ecc496a08
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-11-08 22:12:03 +0500

    fix: cors

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express();
 // CORS setup to allow credentials (cookies)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://localhost:5173","https://conferencesystem.vercel.app"],
     credentials: true,
   })
 );
